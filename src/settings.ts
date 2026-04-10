70a0a461dc68b56991866e5d00b58ab4152ddbff
Ilovepandass <ronanbaxi@gmail.com>
2026-04-10 13:54:58 -0400

    Update settings for rammerhead and URLs

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 const obfuscate = true;
-const rammerhead = true;
-const github = "https://github.com/cognetwork-dev/Metallic";
-const discord = "https://discord.com/invite/yk33HZSZkU";
+const rammerhead = false;
+const github = "https://github.com/Ilovepandass/Metallic";
+const discord = "https://discord.gg/Z2CB8w5t";
 const privacyPolicyUpdated = 1707797140659;
 const gamesCdn = "https://radon.games";
 
